b8cc4a9bc55040d5d6450d5f46bdb0489b0e91ef
toingee <toingee@toingee-VirtualBox.(none)>
2014-04-27 20:22:46 +0800

    Schema edit

--- db/migrate/20101116224140_create_api_keys.rb
+++ db/migrate/20101116224140_create_api_keys.rb
@@ -5,8 +5,8 @@ class CreateApiKeys < ActiveRecord::Migration
       t.integer :device_id
       t.integer :feed_id
       t.integer :user_id
-      t.boolean :write_flag, :default => false
-      t.boolean :public_flag, :default => false
+      t.boolean :write_flag, :default => 0
+      t.boolean :public_flag, :default => 0
 
       t.timestamps
     end
